abe0f1cf0829a3e6571c3f1d8f89334b8e5b1e9a
Gama <gamaliel.g.g131@gmail.com>
2025-01-09 12:20:31 -0600

    getParticipantes, postParticipante

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import express from "express";
-import { json } from "body-parser";
-import rouletteRoutes from "./routes/roulette";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import rouletteRoutes from "./routes/roulette.js";
+import cors from "cors";
 
-const app = express();
+dotenv.config();
+
 const PORT = 3000;
 
-// Middlewares
-app.use(json());
+// Conexión a la base de datos
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("Conectado a mongo"))
+.catch((error) => console.error("Error al conectar a mongo", error));
+
+mongoose.connection.on('error', (err) => {
+  console.log('err', err);
+})
+
+// Iniciar express
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 // Rutas
 app.use("/api/roulette", rouletteRoutes);
--- src/routes/roulette.js
+++ src/routes/roulette.js
@@ -1,14 +1,46 @@
-const express = require('express');
-const router = express.Router();
+import { Router } from 'express';
+import mongoose from 'mongoose';
 
-// Ruta para obtener los nombres
-router.get('/', async (req, res) => {
-  try {
-    const participants = ["Nombre1", "Nombre2", "Nombre3"]; // Esto luego será dinámico
-    res.json(participants);
-  } catch (error) {
-    res.status(500).json({ message: 'Error al obtener los nombres.' });
+const router = Router();
+
+// Modelo para participantes
+const participanteSchema = new mongoose.Schema({
+  nombre: {type: String},
+  fecha: {type: Date},
+  estado: {type: String},
+});
+
+const Participante = mongoose.model('Participante', participanteSchema, 'participantes');
+
+// Ruta para obtener todos los participantes
+router.get("/", async (req, res) => {
+  try{
+    const participantes = await Participante.find({});
+    res.json(participantes);
+  } catch(error){
+    console.error("Error al obtener participantes", error);
+    res.status(500).json({error: "Error al obtener participantes"});
   }
 });
 
-module.exports = router;
+// Ruta para crear un nuevo participante
+router.post("/", async (req, res) => {
+  try{
+    const {nombre} =req.body;
+
+    const nuevoParticipante = new Participante({
+      nombre: nombre,
+      fecha: new Date(),
+      estado: "disponible",
+    });
+
+    const participanteGuardado = await nuevoParticipante.save();
+
+    res.json(participanteGuardado);
+  } catch(error){
+    console.error("Error al guardar participante", error);
+    res.status(500).json({error: "Error al guardar participante"});
+  }
+});
+
+export default router;
