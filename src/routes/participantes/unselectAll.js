18a5c49795ef5efada713dd95323027098a6c07f
Gama <gamaliel.g.g131@gmail.com>
2025-01-15 12:49:32 -0600

    unselectAll hardcodeado

--- src/routes/participantes/unselectAll.js
+++ src/routes/participantes/unselectAll.js
@@ -1,30 +1,29 @@
 import Participante from "../../models/participante.js";
 
-//! deprecated
-// Ruta para actualizar los valores "true" a "false" en el campo "seleccionado"
 // (PATCH) /api/roulette/all
 const resetSeleccionados = async (req, res) => {
   try {
-    const participantes = await Participante.find();
-
-    for (const participante of participantes) {
-      const nuevoSeleccionado = {};
-
-      // Recorrer todas las claves del mapa "seleccionado"
-      for (const [clave, valor] of Object.entries(participante.seleccionado)) {
-        // Si el valor es true, cambiarlo a false, si no, conservarlo
-        nuevoSeleccionado[clave] = valor === true ? false : valor;
+    // Realizar una actualización masiva
+    await Participante.updateMany(
+      {},
+      {
+        $set: {
+          "seleccionado.web": false,
+          "seleccionado.app": false,
+          "seleccionado.all": false,
+        },
       }
+    );
 
-      // Actualizar el mapa "seleccionado" en el documento
-      participante.seleccionado = nuevoSeleccionado;
-      await participante.save();
-    }
-
-    res.json({ message: "Todos los valores 'true' en 'seleccionado' se han cambiado a 'false'." });
+    res.json({
+      message:
+        "Todos los valores 'true' en 'seleccionado' se han cambiado a 'false'.",
+    });
   } catch (error) {
     console.error("Error al actualizar los valores de 'seleccionado'", error);
-    res.status(500).json({ error: "Error al actualizar los valores de 'seleccionado'" });
+    res
+      .status(500)
+      .json({ error: "Error al actualizar los valores de 'seleccionado'" });
   }
 };
 
